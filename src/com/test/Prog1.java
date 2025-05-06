266d52c9ac11488a22a9f190e6f26dc87ad251e4
Anvith Krishna <152867087+AkShadoww@users.noreply.github.com>
2025-05-06 09:31:57 +0530

    Update Prog1.java

---
+++ src/com/test/Prog1.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Prog1{
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+    Scanner s= new Scanner(System.in);
+    System.out.println("Enter the number:\n");
+    int n = s.nextInt();
+    System.out.println("Square of "+ n +"is:"+(n*n));
+	}
+}
